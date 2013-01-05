0929b4aa12386d532e0cfe66bdae3face335055e
Kyle Laplante <kyle.laplante@gmail.com>
2013-01-04 18:56:43 -0800

    edits for if you want to do multiple custom roms without rebooting

--- edit_4_preload.sh
+++ edit_4_preload.sh
@@ -1,4 +1,5 @@
 #!/sbin/sh
 
-sed -i 's/exit/unzip\ -p\ \$3\ \"preload.\*\"\ \|\ tar\ x\nexit/g' /res/image-binary
-echo -e "assert(format("/preload"));\nassert(mount("/preload"));" >> /res/image-edify
+sed 's/exit/unzip\ -p\ \$3\ \"preload.\*\"\ \|\ tar\ x\nexit/g' /res/image-binary > /res/image-binary2
+cp /res/image-edify /res/image-edify2
+echo -e "assert(format("/preload"));\nassert(mount("/preload"));" >> /res/image-edify2
